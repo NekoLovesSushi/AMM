987a8f19c0ed172b1c7446acbb3e30b0f551620f
NekoLovesSushi <eleanor.stein777@gmail.com>
2024-11-26 23:06:14 -0500

    6. AMM Front-End

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,11 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.9", // Ensure this matches your contracts' version
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545", // Local Hardhat Node URL
+    },
+  },
 };
+
